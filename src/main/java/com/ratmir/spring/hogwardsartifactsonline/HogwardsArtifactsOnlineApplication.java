424b214daca063c74cd93b9c1fa665aa9bd4840b
Ratmir <ratmirvslvvslv@gmail.com>
2025-02-14 14:44:18 +0500

    Initial commit

---
+++ src/main/java/com/ratmir/spring/hogwardsartifactsonline/HogwardsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.ratmir.spring.hogwardsartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwardsArtifactsOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwardsArtifactsOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ratmir/spring/hogwardsartifactsonline/HogwardsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratmir.spring.hogwardsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwardsArtifactsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
